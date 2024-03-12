e8f7f2aacc580987b9d20e1e361172cb1b62ce3b
1354174190qjw@sina.com <1354174190qjw@sina.com>
2024-03-12 20:09:14 +0800

    add logback.xml

--- springboot-simple-demo/services/user-function/src/main/java/com/demo/user/function/impl/UserFunctionImpl.java
+++ springboot-simple-demo/services/user-function/src/main/java/com/demo/user/function/impl/UserFunctionImpl.java
@@ -2,10 +2,13 @@ package com.demo.user.function.impl;
 
 import com.springboot.simple.demo.core.entity.api.params.UserLoginParam;
 import com.springboot.simple.demo.core.function.api.user.UserFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserFunctionImpl implements UserFunction {
+
     @Override
     public void userLogin(UserLoginParam param) {
         System.out.println();
